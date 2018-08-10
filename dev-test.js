6cb398fe8399890b8f4a0eed77681732162e9222
JoyJazper <pjk096@gmail.com>
2018-08-10 17:01:19 +0530

    First Commit! Block and SHA256 Implemented.

---
+++ dev-test.js
@@ -0,0 +1,8 @@
+const Block = require('./block');
+const block2 = new Block();
+const block1 = new Block('foo', 'bar', 'zoo', 'baz');
+//console.log(block1.toString());
+//console.log(block2.toString());
+console.log(Block.genesis().toString());
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
