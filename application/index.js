663184b949fc6847fef0df5417d9eaa3cbf959fe
JoyJazper <pjk096@gmail.com>
2018-08-12 15:52:38 +0530

    Chain Synchronization implemented.

--- application/index.js
+++ application/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
+const P2pServer = require('./p2p-server');
+
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -16,6 +20,9 @@ app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()}`);
     res.redirect('/blocks');
+    p2pServer.syncChains();
 });;
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
+
+p2pServer.listen();
---
+++ application/p2p-server.js
@@ -0,0 +1,54 @@
+const Websocket = require('ws');
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+class P2pServer {
+    constructor(blockchain) {
+        this.blockchain = blockchain;
+        this.sockets = [];
+    }
+
+    listen() {
+        const server = new Websocket.Server({
+            port: P2P_PORT
+        });
+        server.on('connection', socket => this.connectSocket(socket));
+        this.connectToPeers();
+        console.log(`Listening to Peer-to-Peer connection on socket ${P2P_PORT}`);
+    }
+
+    connectSocket(socket) {
+        this.sockets.push(socket);
+        console.log('Socket connected');
+        this.messageHandler(socket);
+        socket.send(JSON.stringify(this.blockchain.chain));
+        this.sendChain(socket);
+    }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            const socket = new Websocket(peer);
+            socket.on('open', () => this.connectSocket(socket));
+        });
+    }
+
+    messageHandler(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            this.blockchain.replaceChain(data);
+            console.log('data', data);
+        });
+    }
+
+    sendChain(socket) {
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    syncChains() {
+        this.sockets.forEach(socket => {
+            this.sockets.forEach(socket => this.sendChain(socket));
+        });
+    }
+}
+
+module.exports = P2pServer;
