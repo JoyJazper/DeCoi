fa5c3665e2a805c6db943434fb13a29052c7c0ea
JoyJazper <pjk096@gmail.com>
2018-08-19 02:33:58 +0530

    Posting Transaction success.

--- application/index.js
+++ application/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+
+const wallet = new Wallet();
+const tp = new TransactionPool();
 
 const p2pServer = new P2pServer(bc);
 
@@ -16,12 +21,25 @@ app.get('/blocks', (req, res) => {
     res.json(bc.chain);
 });
 
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
 app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()}`);
     res.redirect('/blocks');
     p2pServer.syncChains();
 });;
+
+app.post('/transact', (req, res) => {
+    const {
+        recipient,
+        amount
+    } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    res.redirect('/transactions');
+});
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
 
--- wallet/index.js
+++ wallet/index.js
@@ -1,4 +1,8 @@
-const { INITIAL_BALANCE } = require('../config');
+const Transaction = require('./transaction');
+
+const {
+  INITIAL_BALANCE
+} = require('../config');
 const ChainUtil = require('../chain-util');
 
 class Wallet {
@@ -16,6 +20,23 @@ class Wallet {
 
   sign(dataHash) {
     return this.keyPair.sign(dataHash);
+  }
+
+  createTransaction(recipient, amount, transactionPool) {
+    if (amount > this.balance) {
+      console.log(`Amount: ${amount}, exceeds current balance: ${this.balance}`);
+      return;
+    }
+
+    let transaction = transactionPool.existingTransaction(this.publicKey);
+    if (transaction) {
+      transaction.update(this, recipient, amount);
+    } else {
+      transaction = Transaction.newTransaction(this, recipient, amount);
+      transactionPool.updateOrAddTransaction(transaction);
+    }
+
+    return transaction;
   }
 }
 
